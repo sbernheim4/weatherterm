7af71c18f5d243fea427c20e752c0abf876bad43
Samuel Bernheim <sambernheim@gmail.com>
2017-07-23 14:48:41 -0400

    Use promises for request module

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const Promise = require("bluebird");
+
 const getIP = require('external-ip')();
 const geoip = require('geoip-lite');
-const request = require('request');
+
+const request = Promise.promisify(require("request"));
+
 const chalk = require('chalk');
-const promise = require("bluebird");
 
 
 const apikey = '64ead4818be5e06a1768c92eac673e33';
@@ -16,10 +19,9 @@ getIP((err, ip) => {
 
 	const queryString = `http://api.openweathermap.org/data/2.5/weather?zip=${geo.zip},${geo.country.toLowerCase()}&units=imperial&APPID=${apikey}`;
 
-	request.get(queryString, (err, res, body) => {
-		if (err) throw err;
-
-		let info = JSON.parse(body);
+	request(queryString)
+	.then(function(res)  {
+		let info = JSON.parse(res.body);
 
 		console.log(
 			chalk.bgBlue(` Weather in ${geo.region} => `) +
@@ -27,6 +29,9 @@ getIP((err, ip) => {
 			chalk.bgKeyword('orange')(chalk.black(` Wind: ${info.wind.speed} mph > `)) +
 			chalk.bgRed(` Humidity: ${info.main.humidity}% `)
 		);
-
+	})
+	.catch((err) => {
+		console.log("AN ERROR WAS THROWN");
+		console.log(chalk.red(err));
 	});
 });
