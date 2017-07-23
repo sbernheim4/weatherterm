77770d687db7bd3b314d7555dd44045bcdc7f7ca
Samuel Bernheim <sambernheim@gmail.com>
2017-07-23 00:36:29 -0400

    Remove express

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const getIP = require('external-ip')();
 const geoip = require('geoip-lite');
 const request = require('request');
+const chalk = require('chalk');
 
 const apikey = '64ead4818be5e06a1768c92eac673e33';
 
@@ -10,7 +11,7 @@ getIP((err, ip) => {
     if (err) throw err;
 
 	const geo = geoip.lookup(ip);
-	console.log(geo.zip);
+	console.log(geo);
 
 	const queryString = `http://api.openweathermap.org/data/2.5/weather?zip=${geo.zip},${geo.country.toLowerCase()}&APPID=${apikey}`;
 
