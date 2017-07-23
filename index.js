a26a4164f66db79d1b6798d902e2e405e373fe74
Samuel Bernheim <sambernheim@gmail.com>
2017-07-23 15:14:45 -0400

    Use promises instead of callbacks

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const Promise = require("bluebird");
 
 const getIP = require('external-ip')();
+const getIPTwo = Promise.promisify(require('external-ip')());
 const geoip = require('geoip-lite');
 
 const request = Promise.promisify(require("request"));
@@ -12,26 +13,25 @@ const chalk = require('chalk');
 
 const apikey = '64ead4818be5e06a1768c92eac673e33';
 
-getIP((err, ip) => {
-    if (err) throw err;
-
+getIPTwo()
+.then( ip => {
 	const geo = geoip.lookup(ip);
+	return `http://api.openweathermap.org/data/2.5/weather?zip=${geo.zip},${geo.country.toLowerCase()}&units=imperial&APPID=${apikey}`;
+})
+.then((res) => {
+	return request(res);
+})
+.then(function(res)  {
+	let info = JSON.parse(res.body);
 
-	const queryString = `http://api.openweathermap.org/data/2.5/weather?zip=${geo.zip},${geo.country.toLowerCase()}&units=imperial&APPID=${apikey}`;
-
-	request(queryString)
-	.then(function(res)  {
-		let info = JSON.parse(res.body);
-
-		console.log(
-			chalk.bgBlue(` Weather in ${geo.region} => `) +
-			chalk.bgGreen(` ${Math.floor(info.main.temp)}°F > `) +
-			chalk.bgKeyword('orange')(chalk.black(` Wind: ${info.wind.speed} mph > `)) +
-			chalk.bgRed(` Humidity: ${info.main.humidity}% `)
-		);
-	})
-	.catch((err) => {
-		console.log("AN ERROR WAS THROWN");
-		console.log(chalk.red(err));
-	});
+	console.log(
+		chalk.bgBlue(` Weather in PLACEHOLDER => `) +
+		chalk.bgGreen(` ${Math.floor(info.main.temp)}°F > `) +
+		chalk.bgKeyword('orange')(chalk.black(` Wind: ${info.wind.speed} mph > `)) +
+		chalk.bgRed(` Humidity: ${info.main.humidity}% `)
+	);
+})
+.catch((err) => {
+	console.log("AN ERROR WAS THROWN");
+	console.log(chalk.red(err));
 });
