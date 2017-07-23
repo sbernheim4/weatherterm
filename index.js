ef08ab59ad075bd389e0a6d15b68b0135da91d83
Samuel Bernheim <sambernheim@gmail.com>
2017-07-22 23:43:56 -0400

    Fix eslint config

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const request = require('request');
 
 const apikey = '64ead4818be5e06a1768c92eac673e33';
 
-let ipAddress;
-
 getIP((err, ip) => {
     if (err) throw err;
 
-	var geo = geoip.lookup(ip);
+	const geo = geoip.lookup(ip);
 
-	const country = geo.country.toLowerCase();
 	request.get(`http://api.openweathermap.org/data/2.5/weather?q=London&APPID=${apikey}`, (err, res, body) => {
 		if (err) throw err;
 		console.log('body is', JSON.parse(body));
